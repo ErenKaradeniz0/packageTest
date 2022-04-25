e1778567b00d0e185a8ae8439ff43c96ad3e3f44
wiserenn <83872752+wiserenn@users.noreply.github.com>
2022-04-25 21:24:13 +0300

    Update and rename Week2.java to Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package SecondTermWeek2;
 import SecondTermWeek2.package1.Test1;
 import SecondTermWeek2.package2.Test2;
 
-public class Week2 {
+public class Main {
 
     public static void main(String[] args) {
         Test1 test1 = new Test1();
